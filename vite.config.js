ca0dc9748c9a0381c85b2b316ee8b4195d1746ea
Adonis <adonis.casado@outlook.com>
2024-06-06 14:19:34 -0400

    deployment

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    outDir: 'build',
-  },
-  css: {
-    modules: {
-      localsConvention: "camelCase",
+export default defineConfig(({ mode }) => {
+  // Load environment variables from .env files
+  const env = loadEnv(mode, import.meta.url);
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+    },
+    build: {
+      outDir: 'build',
+    },
+    css: {
+      modules: {
+        localsConvention: "camelCase",
+      }
+    },
+    define: {
+      'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL),
+      'import.meta.env.VITE_SESSION_KEY': JSON.stringify(env.VITE_SESSION_KEY)
     }
   }
-})
+});
